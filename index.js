549a3aa90ac12ba06385e8ae490f799a5aba1844
SRAKIB17 <rakibulssc5@gmail.com>
2022-01-27 21:06:57 +0600

    all done

--- index.js
+++ index.js
@@ -7,8 +7,22 @@ function loopAdd() {
     var address = document.getElementById('address').value;
     var mobile = document.getElementById('mobile').value;
     var link = document.getElementById('link').value;
+    var domain = ['.com', '.be', '.net', '.in', '.edu', '.org']
+    for (const linkVarify of domain) {
+        var linkRgxp = new RegExp(linkVarify, 'gi')
+        var linkTest = link.match(linkRgxp);
+        var linkCheck = ''
+        if (linkTest !== null) {
+            linkCheck = "<a href='"+link+"'>Follow link</a>"
+            break
+        }
+        else{
+            linkCheck = 'Link Invalid';
+        }
+    }
+    
     // person Array
-    var personList = [person, address, mobile, link]
+    var personList = [person, address, mobile, linkCheck];
     var tData = ''
     for (const td of personList) {
         tData += '<td>'+td+'</td>'
